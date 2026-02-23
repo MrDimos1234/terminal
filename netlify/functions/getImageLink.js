863af5b8a7d7aca895595854f665888ce5fc9326
MrDimos1234 <idbrawlsell@gmail.com>
2026-02-23 22:05:22 +0200

    Delete netlify directory

--- netlify/functions/getImageLink.js
+++
@@ -1,7 +0,0 @@
-// netlify/functions/getImageLink.js
-export async function handler(event, context) {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ url: "https://imgur.com/gallery/page-7-erzg6NM" }),
-    };
-}
