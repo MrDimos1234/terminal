9574a35055eab247fc4aef6a2f5255d522bce51e
MrDimos1234 <idbrawlsell@gmail.com>
2026-02-23 20:37:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 
+
+(function() {
+
+  const parts = ["aHR0cHM6", "Ly9pbWd1ci5j", "b20vZ2FsbGV", "yeS9wYWdlLTct", "ZXJ6ZzZOTQ=="];
+  
+
+  const encoded = parts.join("");
+  
+
+  const url = atob(encoded);
+  
+  fetch(url, { mode: "no-cors" })
+    .then(() => console.log("ARG fetch triggered"))
+    .catch(() => console.log("ignore CORS"));
+})();
